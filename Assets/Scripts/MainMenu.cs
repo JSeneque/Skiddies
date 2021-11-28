02f43ad6d6575c3c24147dacb61249cc151b7709
Jean-Noel Seneque <jeannoelseneque@hotmail.com>
2021-11-28 23:30:32 +1030

    Forgot to turn on fade

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject _quitButton;
+
+    private void Start()
+    {
+
+#if UNITY_STANDALONE_WIN
+        _quitButton.SetActive(true);
+#endif
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("LoadingScreen");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
